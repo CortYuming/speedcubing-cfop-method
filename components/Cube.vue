e41d296c9105273a1f1dbc3d24676de469a5293f
ITO Takashi <cortyuming@gmail.com>
2021-12-24 22:26:04 +0900

    test layout

--- components/Cube.vue
+++ components/Cube.vue
@@ -1,35 +1,52 @@
 <template>
-  <v-card>
+  <v-card height="100%">
     <v-card-text class="pb-sm-1">
       <div class="ext-subtitle-1">
-        PLL
+        {{ type }}
       </div>
     </v-card-text>
     <v-card-title class="headline font-weight-black pt-sm-1">
-      02 Ua
+      {{ title}}
     </v-card-title>
     <v-card-text class="pb-sm-1">
       <div class="mx-auto" style="width:220px; height:240px">
-        <script>AnimCube3("move=M2' U' M2' U' M' U2' M2' U2' M' U2'&initrevmove=#&hint=5")</script>
+        <script>AnimCube3("move={{move}}&initrevmove=#&hint=5")</script>
       </div>
     </v-card-text>
     <v-card-subtitle>
       <p>
-        M2' U' M2' U' M' U2' M2' U2' M' U2'
+        {{ move }}
       </p>
       <v-divider></v-divider>
       <p>
         make pattern <br/>
         <span>
-          (M2' U' M2' U' M' U2' M2' U2' M' U2') *2
+          {{ makePattern }}
         </span>
       </p>
     </v-card-subtitle>
   </v-card>
 </template>
 
-<style>
-  .nuxt-logo {
-    height: 180px;
-  }
-</style>
+<script>
+export default {
+  props: {
+    "type": {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    move: {
+      type: String,
+      default: "",
+    },
+    makePattern: {
+      type: String,
+      default: "",
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="6" md="4">
-      <Cube />
+    <v-col
+      v-for="cubing, i in cubingData"
+      :key="i"
+      cols="12" sm="6" md="4" lg="3">
+      <Cube
+        :type="cubing.type"
+        :title="cubing.title"
+        :move="cubing.move"
+        :make-pattern="cubing.makePattern"
+      />
     </v-col>
   </v-row>
 </template>
@@ -9,5 +17,35 @@
 <script>
 export default {
   name: 'IndexPage',
+  data () {
+    return {
+      cubingData: [
+        {
+          'type': 'PLL',
+          title: '01 Ub',
+          move: "M2' U' M U2' M' U' M2'",
+          makePattern: "(M2' U' M U2' M' U' M2') *2",
+        },
+        {
+          'type': 'PLL',
+          title: '02 Ua',
+          move: "M2' U' M2' U' M' U2' M2' U2' M' U2",
+          makePattern: "(M2' U' M2' U' M' U2' M2' U2' M' U2') *2",
+        },
+        {
+          'type': 'PLL',
+          title: '03 H',
+          move: "M2' U' M2' U2' M2' U' M2'",
+          makePattern: "M2' U' M2' U2' M2' U' M2'",
+        },
+        {
+          'type': 'PLL',
+          title: '04 Ab',
+          move: "x (L U' L) D2 (L' U L) D2 L2 x'",
+          makePattern: "(x (L U' L) D2 (L' U L) D2 L2 x') *2",
+        },
+      ]
+    }
+  }
 }
 </script>
