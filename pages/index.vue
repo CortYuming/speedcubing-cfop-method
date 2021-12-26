98cc08489b30204af59a2020f44c6d22a8a2444f
ITO Takashi <cortyuming@gmail.com>
2021-12-26 10:24:57 +0900

    Modify read cubing data

--- pages/index.vue
+++ pages/index.vue
@@ -32,29 +32,8 @@
 </template>
 
 <script>
-const cubingTextList = [
-  "PLL	01Ub	M2' U' M U2' M' U' M2'	(M2' U' M U2' M' U' M2') *2",
-  "PLL	02Ua	M2' U M U2 M' U M2'	(M2' U M U2 M' U M2') *2",
-  "PLL	03H	M2' U' M2' U2' M2' U' M2'	M2' U' M2' U2' M2' U' M2'",
-  "PLL	04Ab	x (L U' L) D2 (L' U L) D2 L2 x'	(x (L U' L) D2 (L' U L) D2 L2 x') *2",
-  "PLL	05Aa	x L2' D2 (L' U' L) D2 (L' U L') x'	(x L2' D2 (L' U' L) D2 (L' U L') x') *2",
-  "PLL	06T	(R U R' U') R' F R2 U' R' U' (R U R') F'	(R U R' U') R' F R2 U' R' U' (R U R') F'",
-  "PLL	07Nb	(L' U R' U2 L U' R) (L' U R' U2 L U' R) U'	((L' U R' U2 L U' R) (L' U R' U2 L U' R) U') *2",
-  "PLL	08Na	(R U' L U2' R' U L') (R U' L U2' R' U L') U	(R U' L U2' R' U L') (R U' L U2' R' U L') U",
-  "PLL	09Z	(M2' U') (M2' U') M' (U2' M2' U2') M' U2'	(M2' U') (M2' U') M' (U2' M2' U2') M' U2'",
-  "PLL	10E	x' (L' U L D') (L' U' L D) (L' U' L D') (L' U L D) x	x' (L' U L D') (L' U' L D) (L' U' L D') (L' U L D) x",
-  "PLL	11V	(R' U R U') (R' f' U' R) U2' (R' U' R U') R' f R	(R' U R U') (R' f' U' R) U2' (R' U' R U') R' f R",
-  "PLL	12F	R' U' F' (R U R' U') R' F R2 (U' R' U') (R U R') U R	R' U' F' (R U R' U') R' F R2 (U' R' U') (R U R') U R",
-  "PLL	13Rb	R2 F R (U R U' R') F' R U2' R' U2' R U	R2 F R (U R U' R') F' R U2' R' U2' R U",
-  "PLL	14Ra	(R U' R' U') (R U R D) (R' U' R D') (R' U2' R') U'	(R U' R' U') (R U R D) (R' U' R D') (R' U2' R') U'",
-  "PLL	15Jb	(R U R') F' (R U R' U') R' F R2 U' R' U'	(R U R') F' (R U R' U') R' F R2 U' R' U'",
-  "PLL	16Ja	(L' U' L) F (L' U' L U) L F' L2' U L U	(L' U' L) F (L' U' L U) L F' L2' U L U",
-  "PLL	17Y	F (R U' R' U') (R U R') F' (R U R' U') (R' F R F')	F (R U' R' U') (R U R') F' (R U R' U') (R' F R F')",
-  "PLL	18Gd	(R U R') (U' D) R2 U' R U' R' U R' U R2 D'	",
-  "PLL	19Gc	R2 U' R U' R U R' U R2 (D' U) (R U' R') D	",
-  "PLL	20Gb	(R' U' R) (U D') R2 U R' U R U' R U' R2 D	",
-  "PLL	21Ga	R2 U R' U R' U' R U' R2 (D U') (R' U R) D'	",
-];
+import {cubingList} from '~/cubingData.ts'
+
 export default {
   name: 'TopPage',
   data() {
@@ -101,7 +80,7 @@ export default {
     clearSearch() {
       this.$router.push({path: '/'})
     },
-    readCubingTextList(textList) {
+    readCubingList(textList) {
       const values = textList.split('\t').map(s => s.trim())
       return {
         'type': values[0],
@@ -111,8 +90,8 @@ export default {
       }
     },
     getCubingData() {
-      return cubingTextList.map(textList => {
-        return this.readCubingTextList(textList)
+      return cubingList.map(textList => {
+        return this.readCubingList(textList)
       })
     }
   },
