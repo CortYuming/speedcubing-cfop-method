b437fac7282fd72b3783d8b82b3b74ff5741e7ca
ITO Takashi <cortyuming@gmail.com>
2022-01-03 00:13:22 +0900

    Modify maxAssetSize

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,5 +75,9 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend(config) {
+      config.performance.maxAssetSize = 600000; // default 250000bytes
+    }
+  },
 }
